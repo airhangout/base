fee4e7373273de6863010418e787a15216f9cc43
zwang33 <zhenwang04@gmail.com>
2013-01-07 16:56:06 -0600

    Revert "Update airhangout/airhangout/settings.py"
    This reverts commit 506686ddf94467e2ef907f31da03ebeb1c5c8de2.

--- airhangout/airhangout/settings.py
+++ airhangout/airhangout/settings.py
@@ -106,7 +106,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/karsaxiao/Desktop/AirHangout/template',
 )
 
 INSTALLED_APPS = (
