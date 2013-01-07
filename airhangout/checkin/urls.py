b32a6ca5ed72782352ce527e50b7bc1bcfa0e83a
pxiao1 <karsa.xiao@gmail.com>
2013-01-07 16:18:27 -0500

    Update airhangout/checkin/urls.py

--- airhangout/checkin/urls.py
+++ airhangout/checkin/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        
     url(r'^checkin/getFlightInfo/$', 'checkin.views.getFlightInfo'),                   
-    url(r'^checkin/getFlightInfo/search/(?P<carrier>[A-Z][A-Z])/(?P<flight>\d+)/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', 'checkin.views.getJson'),
-    url(r'^checkin/getFlightInfo/findfriends/(?P<carrier>[A-Z][A-Z])/(?P<flight>\d+)/$', 'checkin.views.findmyfriend'),
+    url(r'^checkin/getFlightInfo/search/(?P<carrier>\w+)/(?P<flight>\d+)/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', 'checkin.views.getJson'),
+    url(r'^checkin/getFlightInfo/findfriends/(?P<carrier>\w+)/(?P<flight>\d+)/$', 'checkin.views.findmyfriend'),
                        
 )
