a4949902081e408b8086f1641b84a8caebae8b02
zwang33 <zhenwang04@gmail.com>
2013-01-13 20:58:31 -0600

    some problem
    the POST still can not work. I am trying to debug. just upload firstly
    url: 127.0.0.1/login/login

--- airhangout/login/urls.py
+++ airhangout/login/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, url
+import views
 
 urlpatterns = patterns('',
-    url(r'^', 'login.views.index'),
+    #url(r'^', 'login.views.index'),
+	(r'^register/$', views.register),
+	(r'^loginResult/$', views.loginResult),
+	(r'^login/$', views.login),
+	#(r'^registerResult/$', login.views.registerResult),
+	(r'^testResult/$',views.testSession),
 )
--- airhangout/login/views.py
+++ airhangout/login/views.py
@@ -1,4 +1,62 @@
+# Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from models import User
+import md5
+from django.template import RequestContext
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the login index.")
+def login(request):
+	return render_to_response('login/login.html')
+
+def loginResult(request):
+	user_name = request.POST['username']
+	pwd = request.POST['pwd']
+	
+	request.session['IsLogin'] = 0;
+	m = md5.new()
+	m.update(pwd)
+	message = 'username or password is wrong'
+	try:
+		users = User.objects.get(username=user_name)
+	except User.DoesNotExist:
+		pass
+	else:
+		if(users.password == m.hexdigest()):
+			message = u'%s, login now' % user_name
+			request.session['IsLogin'] = 1;
+			request.session['member_id'] = users.id;
+			request.session['member_name'] = users.username
+	
+	return render_to_response('login/loginResult.html', {'username':message}, context_instance=RequestContext(request))
+	
+	
+def register(request):
+	return render_to_response('login/register.html')
+	
+
+def registerResult(request):
+	user_name = request.POST['username']
+	pwd = request.POST['pwd']
+	repwd = request.POST['repwd']
+	message = user_name
+	request.session['IsLogin'] = 0;
+	if(pwd != repwd):
+		message = 'please enter same password'
+	else:
+		m = md5.new()
+		m.update(pwd)
+		try:
+			users = User.objects.get(username=user_name)
+		except User.DoesNotExist:
+			p = User(username=user_name, password=m.hexdigest())
+			p.save()
+			request.session['IsLogin'] = 1
+			request.session['member_id'] = p.id
+			request.session['member_name'] = user_name
+			
+		else:
+			message = "this user exists now"
+	return render_to_response('login/loginResult.html', {'username':message}, context_instance=RequestContext(request))
+	
+def testSession(request):
+	return HttpResponse(request.session['IsLogin'])
